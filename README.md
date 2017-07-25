6737fe380de5e1ee409515c2c6e4535001cbb1cc
fdelia <fabio_d@gmx.net>
2017-07-25 23:13:46 +0200

    started a bit

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Downloader für Bundesgerichtsentscheide
 
-Work in progress...
+Index der Bundesgerichts- (BGE) und EGMR-Entscheide -> [hier](http://www.bger.ch/index/juridiction/jurisdiction-inherit-template/jurisdiction-recht/jurisdiction-recht-leitentscheide1954.htm)
+
+**Bänder**
+* Band I: verschiedene Abteilungen
+* Band II: öff.-recht. Abteilungen
+* Band III: zivilrechtliche Abteilungen
+* Band IV: strafrechtliche Abteilungen
+* Band V: sozialrechtliche Abteilungen
---
+++ main.py
@@ -0,0 +1,16 @@
+import time
+import urllib2
+import json
+from bs4 import BeautifulSoup
+
+
+# Configs
+hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       # 'Accept-Charset': 'de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4,id;q=0.2,nl;q=0.2',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4,id;q=0.2,nl;q=0.2',
+       'Connection': 'keep-alive'}
+
+
+site = BeautifulSoup(urllib2.urlopen("http://www.20min.ch", timeout=10), "html.parser")
