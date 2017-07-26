6e9d70505eca9a89ce77e999d952caffb1f4a8a6
fdelia <fabio_d@gmx.net>
2017-07-26 21:58:04 +0200

    added argument parser

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+import argparse
+
 import time
-import urllib2
+import requests
 import json
 from bs4 import BeautifulSoup
 
@@ -13,11 +16,16 @@ hdr = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKi
        'Connection': 'keep-alive'}
 
 
-site = BeautifulSoup(urllib2.urlopen("http://www.20min.ch", timeout=10), "html.parser")
+#site = BeautifulSoup(urllib2.urlopen("http://www.20min.ch", timeout=10), "html.parser")
 
 
 class Downloader():
     pass
+
+def load_csv(self):
+    """
+    """
+    return data
 
 def get_urls_from_index(index_url):
     """
@@ -30,3 +38,25 @@ def get_data_from_bge_url(bge_url):
     """
     data = {}
     return data
+
+
+def main(argv):
+    # arguments
+    # if csv-file provided, BGE loaded before will be ignored
+    # argument can be year (all if none provided)
+    # argument can be band (all if none provided)
+    parser = argparse.ArgumentParser('Downloads court decision from the Swiss Federal Court and packs them into a csv-file.')
+    parser.add_argument('--all', action='store_true', help='downloads all decisions, overwrites other download parameters')
+    parser.add_argument('-y', '--year', action='store', help='downloads given year')
+    parser.add_argument('-v', '--volume', action='store', help='downloads given volume (default: all)')
+    args = parser.parse_args()
+
+    if args.all is False and args.year is None and args.volume is None:
+        print('No arguments given.\nTo download all use "--all".')
+        sys.exit(2)
+
+    print(args)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
