aa83a71f6c054f813133a1265c32c3c43cd80917
fdelia <fabio_d@gmx.net>
2017-11-18 16:31:10 +0100

    asd

--- d_all.py
+++ d_all.py
@@ -30,35 +30,65 @@ DEBUG = False
 # f = urllib.request.urlopen(req)
 
 
-def build_url(year, volume='I'):
-    """
-    """
-    year = int(year)
-    now = datetime.datetime.now()
-    if year < 1954 or year > now.year:
-        raise ValueError('Year must be >= 1954 and <= current year.')
+# def build_url(year, volume='I'):
+#     """
+#     """
+#     year = int(year)
+#     now = datetime.datetime.now()
+#     if year < 1954 or year > now.year:
+#         raise ValueError('Year must be >= 1954 and <= current year.')
 
-    # 2017 = 143
-    year_nr = (year - 1954) + 80
-    return 'http://relevancy.bger.ch/php/clir/http/index_atf.php?year={}&volume={}&lang=de&zoom=&system=clir'.format(year_nr, volume)
+#     # 2017 = 143
+#     year_nr = (year - 1954) + 80
+#     return 'http://relevancy.bger.ch/php/clir/http/index_atf.php?year={}&volume={}&lang=de&zoom=&system=clir'.format(year_nr, volume)
 
 
-def download_data(year, vol):
-    """
-    """
-    url = build_url(year, vol)
-    data = [] # rows
+# def download_data(year, vol):
+#     """
+#     """
+#     url = build_url(year, vol)
+#     data = [] # rows
 
-    html = str(urllib.request.urlopen(url, timeout=10).read())
-    links = re.findall(r'(?<=<a href=")[^"]*', html)
-    for link in links:
-        if "/php/clir/http/index.php?highlight_docid=atf%3A%2F%2" in link:
-            link = link.replace("&amp;", "&")
-            data.append(download_bge('http://relevancy.bger.ch' + link, year, vol))
+#     html = str(urllib.request.urlopen(url, timeout=10).read())
+#     links = re.findall(r'(?<=<a href=")[^"]*', html)
+#     for link in links:
+#         if "/php/clir/http/index.php?highlight_docid=atf%3A%2F%2" in link:
+#             link = link.replace("&amp;", "&")
+#             data.append(download_bge('http://relevancy.bger.ch' + link, year, vol))
+
+#     return data
+
+def download_from_list(year, month, day):
+    # Give list data into download BGE
+    url = 'https://www.bger.ch/ext/eurospider/live/de/php/aza/http/index_aza.php?date={}{:02}{:02}&lang=de&mode=news'.format(year, month, day)
+    req = urllib.request.urlopen(url, timeout=10)
+    if req.getcode() != 200:
+        return False
+
+    soup = BeautifulSoup(req.read(), "html.parser")
+    tables = soup.findAll('table')
+    if len(tables) < 2:
+        return False
+
+    data = []
+    for tr in tables[1].findAll('tr')[1:]: # first row is header
+        if len(tr.findAll('a')) > 0:
+            tds = tr.findAll('td')
+            entscheid_datum = tds[1].text
+            entscheid_nr = tds[2].text
+            url = tds[2].find('a')['href']
+            sachgebiet = tds[4].text
+        else:
+            sachgebiet_desc = tr.text.replace('\n', '')
+
+            data.append(download_bge(url, 
+                [str(year), str(month), str(day), entscheid_datum, entscheid_nr, sachgebiet, sachgebiet_desc]))
+
 
     return data
 
-def download_bge(url, year, vol):
+
+def download_bge(url, *args):
     """
     """
     if DEBUG: print(url)
@@ -81,7 +111,7 @@ def download_bge(url, year, vol):
 
     content = soup.find("div", {"class": "content"}).text
 
-    return [str(year), vol, bge_name, refs_bges, refs_artikel, content]
+    return args + (content,)
 
 
 def save_data(data, filename):
@@ -130,18 +160,21 @@ def main(argv):
 
 
     # Get data
+    years = [2017]
     for year in years:
-        # if year < 1976: continue
-        data = []
-        print("Year {}".format(year))
+        for month in range(1, 13):
+            print('{} {:02}'.format(year, month))
+            data = []
+            for day in range(1, 32):
+                print('    {:02}'.format(day))
+                d = download_from_list(year, month, day)
+                if d:
+                    data += d
+            print("    {} rows".format(len(data)))
 
-        for vol in volumes:     
-            data += download_data(year, vol)
-            # print("    Vol {}: {} rows".format(vol, len(data[-1])))
-        print("    {} rows".format(len(data)))
-
-        # Save data
-        save_data(data, "data/leiet_{}.csv".format(year))
+            # Save data
+            save_data(data, "data/publ_{}_{}.csv".format(year, month))
+            break
 
 
 if __name__ == "__main__":
